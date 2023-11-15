8bab9f27233856d8c210043f67b04148e5203a6b
bastien707 <bastien.facqueur@efrei.net>
2023-11-15 12:18:48 +0100

    feat: update tas feature

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 taskList = []
 task_id = 0  # Global counter for task IDs
 
-class Task :
+
+class Task:
     def __init__(self, id, description):
         self.id = id
         self.description = description
         self.checked = False
 
     def __str__(self):
-        checked_str = '[' + str(self.checked) + ']' if self.checked == True else '[ ]'
+        checked_str = '[' + str("x") + \
+            ']' if self.checked == True else '[ ]'
         return str(self.id) + ' ' + checked_str + ' ' + self.description
 
-def rpn_reader():
+    def check(self):
+        self.checked = True
+
+
+def task_runner():
     global task_id  # Declare task_id as global so we can modify it
     menu = True
 
-    while menu: 
+    while menu:
         if taskList == []:
             print("No tasks yet")
         else:
             for task in taskList:
                 print(task)
 
-        operator_input = input("Quelle action voulez-vous effectuer ?")
-        operator, _, description = operator_input.partition(' ')
+        operator_input = input("Quelle action voulez-vous effectuer ? " + "\n")
+        operator, _, user_input = operator_input.partition(' ')
 
         if operator == "+":
             task_id += 1  # Increment task_id
-            _add_task_(task_id, description)  # Pass task_id to _add_task_
+            _add_task_(task_id, user_input)  # Pass task_id to _add_task_
+        elif operator == "x":
+            _update_task_(int(user_input))
+
 
 def _add_task_(id: int, description: str) -> Task:
     if description == "":
@@ -35,4 +44,8 @@ def _add_task_(id: int, description: str) -> Task:
     else:
         taskList.append(Task(id, description))  # Pass id to Task
 
-rpn_reader()
+
+def _update_task_(id: int, taskList: list) -> None:
+    for task in taskList:
+        if task.id == id:
+            task.check()
